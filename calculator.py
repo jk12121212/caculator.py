1ede3d2a7a0ef05754404caaf5827e1300372c2f
jk12121212 <jknige30@gmail.com>
2026-08-09 12:28:30 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,11 @@
 print("This only accepts number form(intergers and decimals) and * for times, + for plus, / for divide and - for minus (there is no square root or other).")
+
 first_number = (input ("first number?:"))
 operation = (input ("operation?:"))
 second_number = (input ("second number?:"))
 
 try :
-
-
+    
     if operation == ("*") :
         answer = float(first_number) * float(second_number)
     elif operation == ("+") :
